8146429a8c07e1e5c14dd22af22086a272f1a9b9
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-23 18:31:43 +0200

    code clean and commented

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var list = [];
 //index
 var index = 0 ;
 var display = false;
+
 //function for capitalize first letter of user input name;
 function upperCaseFirstLetter(name){
   return name.charAt(0).toUpperCase() + name.slice(1);
@@ -20,44 +21,50 @@ while( index < array.length){
 function sortArray(){
   //reset index;
   index=0;
+
   let userInput = document.querySelector('#nameinput');
+
   //check if textfield is empty;
   if (userInput.value != "") {
     let name = upperCaseFirstLetter(userInput.value);
+
     //insert the new string into array
     list.push(name);
     array.push(name);
-    //sort;
     list.sort();
-    //clear textfield;
+    //clear textfield
     userInput.value = "";
-    //clear list;
-    let uList = document.getElementById('list');
+
     //find index number of new string;
+    let uList = document.getElementById('list');
+    uList.innerHTML = "";
     let position = list.indexOf(name) + 1 ;
     document.getElementById('orderednum').innerHTML = position;
+
     //while cicle for print out list item;
-    uList.innerHTML = "";
     while(index < list.length){
       uList.innerHTML += "<li>" + list[index] + "</li>";
       index++;
     }
+
     index=0;
     ul = document.getElementById('unlist');
     ul.innerHTML ="";
+
     while(index < array.length){
       ul.innerHTML += "<li>" + array[index] + "</li>";
       index++;
     }
+
     if(!display){
-      moving()
+      appear()
       display = true;
     }
+
     document.getElementById('unorderednum').innerHTML = array.length  ;
   }
 };
 
-
-function moving(){
+function appear(){
   document.querySelector('aside').classList.toggle('show');
 }
